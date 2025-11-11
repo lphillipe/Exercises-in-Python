74d7aaf14c0e9c269b15e894391b9871bb9d87dc
lphillipe <luisphillipe200@gmail.com>
2025-11-11 06:54:32 -0300

    add dnamulti structure

---
+++ dnamulti.py
@@ -0,0 +1,16 @@
+def max_consecutive(dna, seq):
+    maior = 0
+    tamanho = len(seq)
+
+    for i in range(len(dna)):
+        cont = 0
+        j = i
+
+        while j + tamanho <= len(dna) and dna[j:j+tamanho] == seq:
+            cont += 1
+            j += tamanho
+        
+        if cont > maior:
+            maior = cont
+    return maior
+
