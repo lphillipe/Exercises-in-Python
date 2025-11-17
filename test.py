21bd560c598304898d7205580ede24fec0934b46
lphillipe <luisphillipe200@gmail.com>
2025-11-17 18:53:29 -0300

    UPDATE

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ frase = "Luís Phillipe você é foda e capaz de tudo!!"
 print(frase.upper()) # deixa tudo em maisuculo
 
 print(len(frase)) # conta quantos caracteres tem a frase
+
+print(frase[2])
+print(frase.index("L"))
