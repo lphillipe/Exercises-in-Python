b3ae0528e17f7783105e2844b451457201a1c47a
lphillipe <luisphillipe200@gmail.com>
2025-11-19 19:34:52 -0300

    add dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+conversaodemes = {
+    "Jan": "Janeiro",
+    "Feb": "Fevereiro",
+    "Mar": "Março",
+    "Abr": "Abril",
+    "Mai": "Maio",
+    "Jun": "Junho",
+    "Jul": "Julho",
+    "Ago": "Agosto",
+    "Set": "Setembro",
+    "Out": "Outubro",
+    "Nov": "Novembro",
+    "Dez": "Dezembro"
+}
+
+print(conversaodemes.get("Del", "Mês inválido"))
