82f99a0415d1aa8a3e2a29e178cf090ed407f916
lphillipe <luisphillipe200@gmail.com>
2025-11-20 21:55:16 -0300

    update

---
+++ for.py
@@ -0,0 +1,8 @@
+
+friends = ["Luís", "Oceana", "Saria", "Jane"]
+
+for friend in (range(len(friends))):
+    if friends[friend] == "Saria":
+        print("Achei você Saria")
+    else:
+        print("Não é você Saria")
