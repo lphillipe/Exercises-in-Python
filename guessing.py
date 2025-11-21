aca419e5ff8b9d4b61450da22a0f26aa3769a4f5
lphillipe <luisphillipe200@gmail.com>
2025-11-20 21:38:14 -0300

    update guessing

--- guessing.py
+++ guessing.py
@@ -1,8 +1,18 @@
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
 
-while guess != secret_word:
-    guess = input("Digite seu palpite:")
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Digite seu palpite:")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-print("Parabéns! Você adivinhou a palavra secreta")
+if out_of_guesses:
+    print("Você perdeu! Tente novamente.")
+else:
+    print("Você venceu!")
