65428794e2dc418fe07814f89a1ca94bc94a07f5
lphillipe <luisphillipe200@gmail.com>
2025-11-22 13:17:35 -0300

    add class and student

---
+++ class.py
@@ -0,0 +1,5 @@
+from student import student
+
+print("Student Information system")
+student1 = student("Oceana", 29, "Pedagogy", 8.0)
+print(f"Name: {student1.name}, Age: {student1.age}, Major: {student1.major}, Grade: {student1.grade}")
---
+++ student.py
@@ -0,0 +1,7 @@
+
+class student:
+    def __init__(self, name, age, major, grade):
+        self.name = name
+        self.age = age
+        self.major = major
+        self.grade = grade
