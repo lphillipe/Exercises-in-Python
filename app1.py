02ef7f2dc253c9f10368453d8fead19e0a84c3a2
lphillipe <luisphillipe200@gmail.com>
2025-11-23 17:31:19 -0300

    update

--- app1.py
+++ app1.py
@@ -1,4 +1,9 @@
 from chef import chef
+from chinesechef import chinesechef
 
 myChef = chef()
 myChef.make_special_dish()
+
+
+myChineseChef = chinesechef()
+myChineseChef.make_special_dish()
---
+++ chinesechef.py
@@ -0,0 +1,13 @@
+class chinesechef:
+
+    def make_chicken(self):
+        print("The chef makes chicken")
+
+    def make_salad(self):
+        print("The chef makes salad")
+    
+    def make_special_dish(self):
+        print("The chefe makes orange chicken")
+
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
