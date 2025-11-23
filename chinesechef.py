b392c3a84584037520f1c4374ed9386424f7f1ed
lphillipe <luisphillipe200@gmail.com>
2025-11-23 20:11:51 -0300

    update chinese

--- chinesechef.py
+++ chinesechef.py
@@ -1,13 +1,10 @@
-class chinesechef:
+from chef import chef
 
-    def make_chicken(self):
-        print("The chef makes chicken")
+class chinesechef(chef):
 
-    def make_salad(self):
-        print("The chef makes salad")
-    
     def make_special_dish(self):
-        print("The chefe makes orange chicken")
+        print("The chef makes orange chicken")
+
 
     def make_fried_rice(self):
         print("The chef makes fried rice")
