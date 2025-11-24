534de8d4c2887127212a69d810941f424aedb370
lphillipe <luisphillipe200@gmail.com>
2025-11-24 19:12:45 -0300

    update

--- conversiontempeture.py
+++ conversiontempeture.py
@@ -1,12 +1,12 @@
 try:
     Celsius = float(input("Digite a temperatura em Celsius:"))
 
-except:
+except ValueError:
     print("Por favor, insira um valor númerico válido")
     exit()
 
 Fahrenheit = (Celsius * 9/5) + 32
 Kelvin = Celsius + 273.15
 
-print("A temperatura em Fahrenheit é:", Fahrenheit)
-print("A temperatura em Kelvin é:", Kelvin)
+print(f"A temperatura em Fahrenheit é:{Fahrenheit:.2f}")
+print(f"A temperatura em Kelvin é:{Kelvin:.2f}")
