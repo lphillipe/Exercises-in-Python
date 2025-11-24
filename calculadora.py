cd9eb4a4d761d63e85cc23eccb6cf5f04fa70e71
lphillipe <luisphillipe200@gmail.com>
2025-11-24 19:56:08 -0300

    update CALCULADORA

--- calculadora.py
+++ calculadora.py
@@ -6,22 +6,22 @@ try:
     operacao = input("Digite a operação (+, -, *, /): ")
 
     if operacao == '+':
-        resultado = (num1) + (num2)
+        resultado = num1 + num2
         print(f"O resultado da adição é: {resultado}")
 
     elif operacao == '-':
-        resultado = (num1) - (num2)
+        resultado = num1 - num2
         print(f"O resultado da subtração é: {resultado}")
 
     elif operacao == '*':
-        resultado = (num1) * (num2)
+        resultado = num1 * num2
         print(f"O resultado da multiplicação é: {resultado}")
 
     elif operacao == '/':
-        if (num2) == 0:
+        if num2 == 0:
             print("Erro: Divisão por zero não é permitida.")
         else:
-            resultado = (num1) / (num2)
+            resultado = num1 / num2
             print(f"O resultado da divisão é: {resultado}")
 
     else:
