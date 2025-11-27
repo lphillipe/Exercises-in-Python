9f78895177572f8492e7e91687549ddc5436208e
lphillipe <luisphillipe200@gmail.com>
2025-11-27 20:17:28 -0300

    update finished cadastrosimples

--- cadastrosimples.py
+++ cadastrosimples.py
@@ -3,4 +3,10 @@
 nome = input("Digite o nome da pessoa que você quer adicionar na lista: ")
 idade = int(input("Digite a idade dessa pessoa: "))
 
-open("pessoas.txt", "a").write(f"{nome}, {idade}\n")
+with open("pessoas.txt", "a").write(f"{nome}, {idade}\n")
+
+with open("pessoas.txt", "r") as arquivo:
+    print("Lista de pessoas cadastradas:")
+    for linha in arquivo:
+        print(linha.strip())
+
