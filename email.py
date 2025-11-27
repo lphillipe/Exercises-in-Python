7a036c59524f3df9b1bfbcc83c1ba4be5fc7c4b0
lphillipe <luisphillipe200@gmail.com>
2025-11-27 19:21:59 -0300

    finished email.py

--- email.py
+++ email.py
@@ -1,13 +1,26 @@
-def is_mail(texto):
-    if "@" in texto and "." in texto:
-        return True
-    else:
+def is_email(texto):
+    
+    if texto.count("@") != 1:
         return False
+
+    parte1, parte2 = texto.split("@")
+    if parte1 == "":
+        return False
+    elif "." not in parte2:
+        return False
+    
+    elif parte2.endswith("."):
+        return False
+    
+    elif parte2.startswith("."):
+        return False
+
+    return True
 
 
 email_digitado = input("Digite um emaill: ")
 
-resultado = is_mail(email_digitado)
+resultado = is_email(email_digitado)
 
 if resultado == True:
     print("Email válido")
