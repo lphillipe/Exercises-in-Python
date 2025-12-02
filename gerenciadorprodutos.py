0a109c30db3c5bee7529a0dca956d7d5f1d8b6e4
lphillipe <luisphillipe200@gmail.com>
2025-12-01 21:46:28 -0300

    update gerenciador

--- gerenciadorprodutos.py
+++ gerenciadorprodutos.py
@@ -1,2 +1,22 @@
-def produtos:
-    print("Listando produtos...")
+def ler_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+
+        except ValueError:
+            print("Entrada inválida. Por Favor, insira um número inteiro.")
+
+
+def ler_float(prompt):
+    while True:
+        try:
+            return float(input(prompt).replace(',', '.'))
+        except ValueError:
+            print("Por favor, insira um número válido (ex: 12.50).")
+             
+def main():
+    produtos = []
+
+    qtd = ler_int("Quantos produtos você quer cadastras? ")
+
+    for i in range(1, qtd + 1):
