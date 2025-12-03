30cffd8561b88c7931cb79354ff878bb0358af48
lphillipe <luisphillipe200@gmail.com>
2025-12-03 06:19:02 -0300

    update lista

--- lista.py
+++ lista.py
@@ -1,17 +1,25 @@
 
+def analisa_lista(lista):
+    if not lista:
+        return "A lista está vazia."
+    
+    maior = max(lista)
+    menor = min(lista)
+    soma = sum(lista)
+    media = soma / len(lista)
+    sum(1 for n in lista if n > media)
 
-
-
-
-
-
-
-
-
-
+    return {
+        "maior": maior,
+        "menor": menor,
+        "soma": soma,
+        "media": media,
+        "acima da media": sum(1 for n in lista if n > media)
+    }
 
 entrada = input("Digite os números separados por vírgula: ")
 
 lista = [int(x) for x in entrada.split(",")]
 
-print("Lista de números inteiros:", lista)
+resultado = analisa_lista(lista)
+print("Análise da lista: ", resultado)
