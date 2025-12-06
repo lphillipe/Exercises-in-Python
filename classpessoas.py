2da89db786d4384e4d410e1ccfd79dd990036bb6
lphillipe <luisphillipe200@gmail.com>
2025-12-06 10:25:49 -0300

    add classpessoas

---
+++ classpessoas.py
@@ -0,0 +1,17 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def maior_de_idade(self):
+        if self.idade >= 18:
+            return True
+        else:
+            return False
+
+
+p1 = Pessoa("Luís", 30)
+p2 = Pessoa("Oceana", 29)
+p3 = Pessoa("Saria", 8)
+
+pessoas = [p1, p2, p3]
