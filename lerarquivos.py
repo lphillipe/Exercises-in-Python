f9ab893227256b32dc677931f11ca6172ec75eca
lphillipe <luisphillipe200@gmail.com>
2025-12-08 10:05:44 -0300

    finished exercise lerarquivos

--- lerarquivos.py
+++ lerarquivos.py
@@ -4,8 +4,21 @@ with open("texto2.txt", "w") as arquivo:
     arquivo.write(texto)
 
 with open("texto2.txt", "r") as arquivo:
-    arquivo.read()
+    conteudo = arquivo.read()
 
-palavras = texto.split()
+linhas = conteudo.splitlines()
+qtd_linhas = len(linhas)
+
+palavras = conteudo.split()
+qtd_palavras = len(palavras)
+
+caracteres_com_espaco = len(conteudo)
+caracteres_sem_espaco = len(conteudo.replace(" ", ""))
 
 mais_longa = max(palavras, key=len)
+
+print("Quantidades de linhas", qtd_linhas)
+print("Quantidade de palavras", qtd_palavras)
+print("Quantidade de caracteres com espaço", caracteres_com_espaco)
+print("Quantidade de caracteres sem espaço", caracteres_sem_espaco)
+print("Palavra mais longa", mais_longa)
