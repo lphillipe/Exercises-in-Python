33fdc59ca068ecb58b7f6463a16570868d23aba2
lphillipe <luisphillipe200@gmail.com>
2025-12-10 10:28:23 -0300

    finish construtor4

--- construtor4.py
+++ construtor4.py
@@ -1,23 +1,35 @@
 class Conta:
     def __init__(self, titular, saldo):
         self.titular = titular
-        self.saldo = saldo
+        self.saldo = saldo if saldo >= 0 else 0
 
     def depositar(self, valor):
         if valor <=0:
-            print("Valor inválido!")
-        else:
-            return self.saldo += valor
+            print("Valor inválido")
+            return False
+        self.saldo += valor
+        print(f"Deposito de {valor:.2f} concluído")
+        return True
     
     def sacar(self, valor):
         if valor <=0:
-            print("O valor não pode ser negativo.")
-        elif valor > self.saldo:
-            print("O valor de saque e maior que o saldo.")
-        else:
-            return self.saldo -= valor
+            print("Valor não pode ser negativo")
+            return False
+        if valor > self.saldo:
+            print("Valor não pode ser maior que o saldo")
+            return False
+        self.saldo -= valor
+        print(f"Saque de R$ {self.saldo:.2f} efetuado com sucesso")
+        return True
 
     def extrato(self):
-        print("Seu saldo atual é", self.saldo)
+        print(f"Seu saldo atual é: R$ {self.saldo:.2f}")
+        return self.saldo
 
+
+c = Conta("Luís", 3100)
+
+c.depositar(500)
+c.sacar(1000)
+c.extrato()
     
