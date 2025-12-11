cc045d363ba6b6f09fb81ddc5649bf53adbdb9ec
lphillipe <luisphillipe200@gmail.com>
2025-12-11 16:50:19 -0300

    fi8nish construtor8

--- construtor8.py
+++ construtor8.py
@@ -1,2 +1,23 @@
 class Personagem:
-    def __init__(self, nome, classe, vida=100, mana=50)
+    def __init__(self, nome, classe, vida=100, mana=50):
+        self.nome = nome
+        self.classe = classe
+        self.vida = vida
+        self.mana = mana
+
+    def status(self):
+        print("==== STATUS ====")
+        print(f"Nome: {self.nome}")
+        print(f"Classe: {self.classe}")
+        print(f"Vida: {self.vida}")
+        print(f"Mana: {self.mana}")
+        print("================")
+
+
+p1 = Personagem("AmericanO", "Blade Knight", 32767, 32767)
+p2 = Personagem("StyleFox", "Mago", 80, 1000)
+p3 = Personagem("Darkness", "Merlin")
+
+p1.status()
+p2.status()
+p3.status()
