2fa1dc6a869d14e70d0caaec899e5304ab878d16
lphillipe <luisphillipe200@gmail.com>
2025-12-11 16:22:21 -0300

    finish construtor7

--- construtor7.py
+++ construtor7.py
@@ -1,5 +1,5 @@
 class Livro:
-    def __init___(self, nome, autor):
+    def __init__(self, nome, autor):
         self.nome = nome
         self.autor = autor
 
@@ -16,9 +16,29 @@ class Biblioteca:
         self.livros.append(livro)
 
     def buscar_por_autor(self, autor):
-        pass
+        encontrados = []
+        for livro in self.livros:
+            if livro.autor == autor:
+                encontrados.append(livro)
+        
+        return encontrados
 
 
     def listar(self):
         for livro in self.livros:
             print(f"Nome do livro é {livro.nome} e seu autor é {livro.autor}")
+
+l1 = Livro("Dom Casmurro", "Machado de Assis")
+l2 = Livro("Memórias Póstumas", "Machado de Assis")
+l3 = Livro("Capitães da Areia", "Jorge Amado")
+
+bib = Biblioteca("Central")
+
+bib.adicionar_livro(l1)
+bib.adicionar_livro(l2)
+bib.adicionar_livro(l3)
+
+resultado = bib.buscar_por_autor("Machado de Assis")
+
+for livro in resultado:
+    print(livro.nome)
