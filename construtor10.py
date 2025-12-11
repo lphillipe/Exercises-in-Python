f8332f633b68196407337af7e90f14f2375ca27e
lphillipe <luisphillipe200@gmail.com>
2025-12-11 17:38:47 -0300

    update def autenticar

--- construtor10.py
+++ construtor10.py
@@ -3,7 +3,18 @@ class Senha:
         if not isinstance(senha, str):
             raise TypeError("A Senha precisa ser uma string")
 
+        if len(senha) < 6:
+            raise ValueError("A senha deve ter pelo menos 6 caracteres")
+
         self.nome = nome
         self.senha = senha
 
-u = Senha("Ana", 123)
+    def autenticar(self, senha_digitada):
+        senha_digitada == self.senha
+
+
+
+u = Senha("Ana", "123456")
+
+print(u.autenticar("123456"))
+print(u.autenticar("errada"))
