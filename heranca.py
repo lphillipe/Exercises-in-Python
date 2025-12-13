8ae8df5063a931ebcb7afa3934c187446ef2ba9a
lphillipe <luisphillipe200@gmail.com>
2025-12-12 21:25:50 -0300

    add class gerente in heranca

--- heranca.py
+++ heranca.py
@@ -5,3 +5,8 @@ class Funcionario:
 
     def dados(self):
         print(f"Voce se chama {self.nome} e seu salário é {self.salario}")
+
+class Gerente(Funcionario):
+    def __init__(self, nome, salario, departamento):
+        super().__init__(nome, salario)
+        self.departamento = departamento
