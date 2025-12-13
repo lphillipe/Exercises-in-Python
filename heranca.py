176b2dfe21ff42a463181d1eb6983a528d490547
lphillipe <luisphillipe200@gmail.com>
2025-12-12 21:31:08 -0300

    finishs heranca

--- heranca.py
+++ heranca.py
@@ -10,3 +10,14 @@ class Gerente(Funcionario):
     def __init__(self, nome, salario, departamento):
         super().__init__(nome, salario)
         self.departamento = departamento
+
+    def dados(self):
+        super().dados()
+        print(f"Departamento:{self.departamento}")
+
+
+f = Funcionario("Luís", 3000)
+g = Gerente("Oceana", 5000, "Pedagogia")
+
+f.dados()
+g.dados()
