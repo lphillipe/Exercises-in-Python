632c8be14d7c8bbe0413837c2a6735d1949cfa17
lphillipe <luisphillipe200@gmail.com>
2025-12-17 20:16:04 -0300

    finish http5

--- api_estudos/http_básico4.py
+++ api_estudos/http_básico4.py
@@ -1,5 +1,11 @@
 import requests
 
 url = "https://jsonplaceholder.typicode.com/posts"
+url_fake = "https://jsonplaceholder.typicode.com/postsss"
 
-url_tratada = requests.get(url)
+url_tratada = requests.get(url_fake)
+
+if url_tratada.status_code == 200:
+    print("Sucesso")
+else:
+    print("Erro")
