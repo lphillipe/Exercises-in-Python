3df4bf97b788cfa08c9321963368fa74eb11543c
lphillipe <luisphillipe200@gmail.com>
2025-12-17 19:54:48 -0300

    update http3

--- api_estudos/http_basico3.py
+++ api_estudos/http_basico3.py
@@ -5,3 +5,7 @@ url = "https://jsonplaceholder.typicode.com/posts"
 url_tratada = requests.get(url)
 
 dados = url_tratada.json()
+
+for dado in dados:
+    print(dado["id"])
+    print(dado["title"])
