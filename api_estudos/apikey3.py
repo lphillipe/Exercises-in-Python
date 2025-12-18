0420f7e1d694562f98465b4c30aa62fbb79e3f36
lphillipe <luisphillipe200@gmail.com>
2025-12-18 19:33:36 -0300

    finish apikey3

--- api_estudos/apikey3.py
+++ api_estudos/apikey3.py
@@ -10,3 +10,10 @@ headers = {
 url = "https://jsonplaceholder.typicode.com/posts"
 
 response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print("Successful Authorization")
+elif response.status_code in (401, 403):
+    print("Wrong Authorization")
+else:
+    print("Other errors")
