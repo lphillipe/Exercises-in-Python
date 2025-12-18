9925bf629bf1c81a0474179e2a7541945a719b27
lphillipe <luisphillipe200@gmail.com>
2025-12-18 18:36:34 -0300

    change apikey2

--- api_estudos/apikey2.py
+++ api_estudos/apikey2.py
@@ -9,3 +9,12 @@ headers = {
 url = "https://jsonplaceholder.typicode.com/posts"
 
 response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print("Sucessful Authorization")
+
+elif response.status_code == in (401, 403):
+    print("Wrong authorization")
+
+else:
+    print("Others errors")
