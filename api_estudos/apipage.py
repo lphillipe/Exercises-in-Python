6082a8414f332fe3bff11ecf3bdc9d563dbe36de
lphillipe <luisphillipe200@gmail.com>
2025-12-18 19:54:01 -0300

    finish apipage

--- api_estudos/apipage.py
+++ api_estudos/apipage.py
@@ -1,6 +1,19 @@
 import requests
 
-url ="https://jsonplaceholder.typicode.com/posts"
-
 page = 1
 limit = 5
+
+params = {
+    "_page": page,
+    "_limit": limit
+}
+
+url ="https://jsonplaceholder.typicode.com/posts"
+
+response = requests.get(url, params=params)
+
+datas = response.json()
+
+for data in datas:
+    print(data["id"])
+    print(data["title"])
