21cb8ed0e764dffc76fc188fc6959c72df1af70f
lphillipe <luisphillipe200@gmail.com>
2025-12-18 18:23:49 -0300

    finish apikey

--- api_estudos/apikey.py
+++ api_estudos/apikey.py
@@ -1,5 +1,18 @@
+import requests
+
 API_KEY = "My Key"
 
 headers = {
     "Authorization": API_KEY
 }
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print("Sucesso")
+elif response.status_code == 401 or response.status_code == 403:
+    print("Erro de Autenticação")
+else:
+    print("Outros erros")
