fb68b25a8a6564e0f7dc6266ab10a5e7ba25a3fe
lphillipe <luisphillipe200@gmail.com>
2025-12-21 19:07:01 -0300

    update project

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -9,4 +9,9 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    return render_template('market.html')
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Notebook', 'barcode': '123965473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150},
+    ]
+    return render_template('market.html', items=items)
