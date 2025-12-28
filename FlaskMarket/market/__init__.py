a220583579808f20ff4b3d4153cdbae06d3b1fcf
lphillipe <luisphillipe200@gmail.com>
2025-12-28 18:54:27 -0300

    Update models, forms, init, routes and create template login and update base.

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = 'd57032b601e0868aa8525b06'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
