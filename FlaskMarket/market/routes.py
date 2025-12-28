90a6002dd591aa05c47258c7410b789e80defac5
lphillipe <luisphillipe200@gmail.com>
2025-12-28 16:58:45 -0300

    update project

--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -19,13 +19,15 @@ def market_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+        user_to_create = User(username=form.username.data, 
+                              email_address=form.email_address.data, 
+                              password_hash=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
 
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
       
     return render_template('register.html', form=form)
