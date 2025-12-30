bc5d16ad6c0e909ebf241c97efd4c45b42cdd545
lphillipe <luisphillipe200@gmail.com>
2025-12-30 16:27:58 -0300

    update def login_page insert flask_login parameters in routes

--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/home')
@@ -37,5 +38,13 @@ def login_page():
     form = LoginForm()
     if form.validate_on_submit():
         attempted_user = User.query.get(form.username.data).first()
-        if attempted_user
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+            ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as:  {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please Try again', category='danger')
+
     return render_template('login.html', form=form)
