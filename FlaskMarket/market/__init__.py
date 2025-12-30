66ab44135a2d2100882288fd1d01562ca055c670
lphillipe <luisphillipe200@gmail.com>
2025-12-30 16:11:28 -0300

    add flask_login in __init__

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = 'd57032b601e0868aa8525b06'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
