f26185905c10e8e04656d7b4c9f1527da0c7f4e6
lphillipe <luisphillipe200@gmail.com>
2025-12-30 16:39:06 -0300

    add method Usermixin in models

--- FlaskMarket/market/models.py
+++ FlaskMarket/market/models.py
@@ -1,11 +1,12 @@
 from market import db, login_manager
 from market import bcrypt
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user.id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
