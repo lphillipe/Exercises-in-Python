949485889150e133df2f05d610927203509035d4
lphillipe <luisphillipe200@gmail.com>
2025-12-31 15:39:12 -0300

    add message success create account in routes

--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -26,6 +26,8 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
 
     if form.errors != {}:
