6759f85318fcee1d2f444a476be875ba5be7de1d
lphillipe <luisphillipe200@gmail.com>
2026-01-03 19:09:40 -0300

    finished the selling logic implemetation and finished the project

--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -16,6 +16,7 @@ def market_page():
     purchase_form = PurchaseItemForm()
     selling_form = SellItemForm()
     if request.method == "POST":
+        #Purchase Item Logic
         purchased_item = request.form.get('purchased_item')
         p_item_object = Item.query.filter_by(name=purchased_item).first()
         if p_item_object:
@@ -24,6 +25,16 @@ def market_page():
                 flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price}$", category='success')
             else:
                 flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}", category='danger')
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+
 
         return redirect(url_for('market_page'))
     
