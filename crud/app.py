491f905af05860b170a94650af173d76db048637
lphillipe <luisphillipe200@gmail.com>
2026-01-13 07:07:05 -0300

    add def create user in app py and create a user for test

--- crud/app.py
+++ crud/app.py
@@ -1,4 +1,21 @@
 from database import engine, Base
 from models import User
 
-Base.metadata.create_all(engine)
+def create_user():
+    db = SessionLocal()
+    
+    user = User(
+        username="Luis Phillipe", 
+        email="phillipe@gmail.com", 
+        password_has="123456"
+    )
+
+    db.add(user)
+    db.commit()
+
+    print(f"Usuário criado com ID {user.id}")
+
+    db.close()
+
+if __name__== "__main__":
+    create_user()
