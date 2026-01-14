db7dc584306f49c5d2713c02dba4e0bff1af5d76
lphillipe <luisphillipe200@gmail.com>
2026-01-14 07:25:17 -0300

    testing functions

--- crud/app.py
+++ crud/app.py
@@ -1,29 +1,7 @@
 from database import SessionLocal
 from models import User
 
-#def create_user(username, email):
-    db = SessionLocal()
 
-    #error handling
-    user_exists = db.query(User).filter_by(email=email).first()
-
-    if user_exists:
-        print(f"Email já cadastrado")
-        db.close()
-        return
-    
-    #Create Users
-    user = User(
-        username=username, 
-        email=email, 
-        password_hash="123456"
-    )
-    db.add(user)
-    db.commit()
-
-    print(f"Usuário criado com ID {user.id}")
-
-    db.close()
 def get_all_users():
     db = SessionLocal()
     users = db.query(User).all()
@@ -55,5 +33,8 @@ def get_user_by_email(email):
         print("Email não encontrado")
 
     db.close()
-    
+
 if __name__== "__main__":
+    get_all_users()
+    get_user_by_id(3)
+    get_user_by_email("phillipe@gmail.com")
