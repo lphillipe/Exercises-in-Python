9a129c0995ce699c1a9af4877b7a204d955f439d
lphillipe <luisphillipe200@gmail.com>
2026-01-16 06:35:49 -0300

    add and testing function update in app

--- crud/app.py
+++ crud/app.py
@@ -2,39 +2,22 @@ from database import SessionLocal
 from models import User
 
 
-def get_all_users():
-    db = SessionLocal()
-    users = db.query(User).all()
-
-    for user in users:
-        print(user.id, user.username, user.email)
-    db.close()
-
-def get_user_by_id(user_id):
+def update_user_email(user_id, new_email):
     db = SessionLocal()
 
     user = db.query(User).filter_by(id=user_id).first()
 
-    if user:
-        print(user.username, user.email)
-    else:
+    if not user:
         print("Usuário não encontrado")
+        db.close()
+        return
+
+    user.email = new_email
+
+    db.commit()
+
+    print("Email atualizado com sucesso")
 
     db.close()
 
-def get_user_by_email(email):
-    db = SessionLocal()
-
-    user = db.query(User).filter_by(email=email).first()
-
-    if user:
-        print(user.id, user.username)
-    else:
-        print("Email não encontrado")
-
-    db.close()
-
-if __name__== "__main__":
-    get_all_users()
-    get_user_by_id(3)
-    get_user_by_email("phillipe@gmail.com")
+# testando
