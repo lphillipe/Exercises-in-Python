14b7ea479314da13e85dcd7e59a05ed3342e295a
lphillipe <luisphillipe200@gmail.com>
2026-01-19 07:27:46 -0300

    create function update_user_email

--- user_management/crud.py
+++ user_management/crud.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
 from models import User
 
+
+#Function Create
+
 def create_user(db: Session, username: str, email: str, password_hash: str):
     
     # Step 1: check username
@@ -23,6 +26,7 @@ def create_user(db: Session, username: str, email: str, password_hash: str):
 
     return user
 
+#Function Read
 def get_all_users(db: Session):
     users = db.query(User).all()
     return users
@@ -31,5 +35,28 @@ def get_user_by_id(db: Session, user_id: int):
     user = db.query(User).filter_by(id=user_id).first()
     return user
 
+def get_user_by_username(db: Session, username: str):
+    user = db.query(User).filter_by(username=username).first()
+    return user
+
+#Function Update
+
+def update_user_email(db: Session, user_id: int, new_email: str):
+    user = db.query(User).filter_by(id=user_id).first()
+
+    if user is None:
+        return None
+
+    email_exists = db.query(User).filter_by(email=new_email).first()
+
+    if email_exists:
+        return None
+
+    user.email = new_email
+
+    db.commit()
+
+    return user
 
 
+    
