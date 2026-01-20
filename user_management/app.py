05b765f46367bae0f73c06c0f08ccdfa128cb6c1
lphillipe <luisphillipe200@gmail.com>
2026-01-20 16:03:08 -0300

    add imports in app.py

---
+++ user_management/app.py
@@ -0,0 +1,10 @@
+from database import engine, SessionLocal, Base
+from crud import (
+    create_user,
+    get_all_users,
+    get_user_by_id,
+    update_user_username,
+    update_user_email,
+    delete_user_by_id,
+    delete_user_by_username
+)
