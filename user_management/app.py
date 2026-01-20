df21a1d65cd3ea4172bef0cdff76fdf189df7c62
lphillipe <luisphillipe200@gmail.com>
2026-01-20 16:13:51 -0300

    testing read

--- user_management/app.py
+++ user_management/app.py
@@ -16,11 +16,24 @@ def main():
     db = SessionLocal()
 
     #Testing CREATE
-    user1 = create_user(db, "Joaquim", "joaquim@gmail.com", "123")
-    user2 = create_user(db, "Maria", "maria@gmail.com", "123")
-    user3 = create_iser(db, "Joaquim", "duplicado@gmail.com", "123")
+    user1 = create_user(db, "joaquim", "joaquim@gmail.com", "123")
+    user2 = create_user(db, "maria", "maria@gmail.com", "123")
+    user3 = create_iser(db, "joaquim", "duplicado@gmail.com", "123")
 
     print(user1)
     print(user2)
     print(user3)
+
+    #Testing READ
+
+    print("\n Todos os usuários:")
+    users = get_all_users(db)
+    for u in users:
+        print(u.id, u.username, u.email)
+
+    print("\n Buscar por ID:")
+    print(get_user_by_id(db, 1))
+
+    print("\n Buscar por username:")
+    print(get_user_by_username(db, "maria"))
     
