79b79f81dacc43c8042e86985b28faac49559aa5
lphillipe <luisphillipe200@gmail.com>
2026-01-20 16:34:25 -0300

    update database

--- user_management/database.py
+++ user_management/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import Session
 
 DATABASE_URL = "sqlite:///database.db"
 
@@ -15,3 +16,10 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
