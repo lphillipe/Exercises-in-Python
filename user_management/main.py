1fa6c8f0ae93c60a11a217a816182a3c1a5a4876
lphillipe <luisphillipe200@gmail.com>
2026-01-22 06:41:12 -0300

    create endpoint post

--- user_management/main.py
+++ user_management/main.py
@@ -11,3 +11,22 @@ import schemas
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="User Management API")
+
+# Endpoint POST
+@app.post("/users", response_model=schemas.UserResponse)
+
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    created_user = crud.create_user(
+        db,
+        username=user.username,
+        email=user.email,
+        password_hash=user.password
+    )
+
+    if not created_user:
+        raise HTTPException(status_code=400, detail="Username or email already exists")
+    
+    return created_user
+
+# Endpoint GET
+@app.get("/users", response_model=list)
