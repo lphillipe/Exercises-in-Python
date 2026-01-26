3e4100f38400a024a7f1f0ddeb9617f9a9167265
lphillipe <luisphillipe200@gmail.com>
2026-01-26 06:41:31 -0300

    create function access_token in security.py

--- user_management/security.py
+++ user_management/security.py
@@ -1,4 +1,17 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+
+
+SECRET_KEY= "123"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
