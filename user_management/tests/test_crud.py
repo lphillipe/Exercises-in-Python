f98aba95168c6c57aa086c36b50894b4799873f1
lphillipe <luisphillipe200@gmail.com>
2026-01-27 07:22:50 -0300

    create test_crud and create function test

---
+++ user_management/tests/test_crud.py
@@ -0,0 +1,32 @@
+from crud import create_user, authenticate_user
+
+def test_create_user_success(db):
+    user = create_user(
+        db,
+        username="joao"
+        email="joao@gmail.com"
+        password="123"
+    )
+
+    assert user is not None
+    assert user.id is not None
+    assert user.username == "joao"
+
+def test_create_user_duplicate_email(db):
+    create_user(db, "joao", "joao@gmail.com", "123")
+    user = create_user(db, "maria", "joao@gmail.com", "123")
+    
+    assert user is None
+
+def test_authenticate_user_success(db):
+    create_user(db, "joao", "joao@gmail.com", "123")
+    user = authenticate_user(db, "joao@gmail.com", "123")
+
+    assert user is not None
+
+def test_authenticate_user_wrong_password(db):
+    create_user(db, "joao", "joao@gmail.com", "123")
+
+    user = authenticate_user(db, "joao@gmail.com", "wrong")
+
+    assert user is None
