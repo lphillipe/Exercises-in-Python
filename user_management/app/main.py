cf823b83e39d5df33271cc83fd5cc719d93bb539
lphillipe <luisphillipe200@gmail.com>
2026-01-29 07:56:54 -0300

    adjuste main endpoint login

--- user_management/app/main.py
+++ user_management/app/main.py
@@ -7,15 +7,18 @@ from app import crud, schemas, security
 from app.security import get_current_user
 from app.models import User
 
+from fastapi.security import OAuth2PasswordRequestForm
 
-#Create tables
 
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="User Management API")
 
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
 # Endpoint POST Create USERS
-@app.post("/users", response_model=schemas.UserResponse)
+@app.post("/users", response_model=schemas.UserResponse, status_code=201)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     created_user = crud.create_user(
         db,
@@ -45,7 +48,7 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     return user
 
 @app.put("/users/{user_id}/username", response_model=schemas.UserResponse)
-def update_username(user_ind: int, username: str, db: Session = Depends(get_db)):
+def update_username(user_id: int, username: str, db: Session = Depends(get_db)):
     user = crud.update_user_username(db, user_id, username)
 
     if not user:
@@ -60,11 +63,15 @@ def delete_user(user_id: int, db: Session = Depends(get_db), current_user: User 
     if not deleted:
         raise HTTPException(status_code=404, detail="User not found")
 
-    return {"message": "User found successfully"}
+    return {"message": "User deleted successfully"}
 
 @app.post("/login")
-def login(data: schemas.LoginData, db: Session = Depends(get_db)):
-    user = crud.authenticate_user(db, data.email, data.password)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = crud.authenticate_user(
+        db,
+        email=form_data.username,
+        password=form_data.password
+    )
 
     if not user:
         raise HTTPException(status_code=401, detail="Invalid Credentials")
