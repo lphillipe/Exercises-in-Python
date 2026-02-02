5ee498bc171208fc3f6201269285c0213ba06c24
lphillipe <luisphillipe200@gmail.com>
2026-02-02 06:45:58 -0300

    add type hints and examples

---
+++ projects/fast_master/examples/type_hints.py
@@ -0,0 +1,13 @@
+def saudacao(name: str, age: int) -> str:
+    return f"Olá, {name}, sua idade é {age}"
+
+saudacao('Luis', 31)
+
+
+idade: int = 31
+orecos: list[float] = [10.5, 20.3]
+
+
+class User:
+    name: str
+    age: int
