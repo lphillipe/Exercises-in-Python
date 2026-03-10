c78f2c3b3d0fc94c4e8bcec88ff8c6d45fdc8888
lphillipe <luisphillipe200@gmail.com>
2026-03-10 10:44:35 -0300

    add condicionais_2

---
+++ condicionais_2.py
@@ -0,0 +1,10 @@
+linguagem = input('Digite uma linguagem: ')
+
+if linguagem == 'Python':
+    print('Python é legal!')
+elif linguagem == 'Java':
+    print('Java é dificil!')
+elif linguagem == 'Cobol':
+    print('Cobol é antigo!')
+else:
+    print('Linguagem não identificada!')
