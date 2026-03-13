9bd1db59d2cbf6a6e4206df61b6ee3084ca44000
lphillipe <luisphillipe200@gmail.com>
2026-03-13 07:52:53 -0300

    update list_1

--- lista_1.py
+++ lista_1.py
@@ -3,3 +3,11 @@ lista = ['Um', 'Dois', 'Três']
 print(lista[0])
 
 meu_dict = {'nome': 'Phillipe', 'idade': 31, 'profissao': 'Desenvolvedor'}
+
+print(meu_dict)
+
+print(meu_dict['nome'])
+
+
+
+print(meu_dict.get('profissao'))
