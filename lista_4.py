ce231dd42e40daba45b74ce16467fe693173cc0c
lphillipe <luisphillipe200@gmail.com>
2026-03-15 14:48:18 -0300

    add _lista4

--- lista_4.py
+++ lista_4.py
@@ -1,5 +1,5 @@
 nomes = ['Ana', 'Felipe', 'João', 'Arlindo', 'Carlos']
 
-nomes_maiusculos = [nome.upper() for nome in nomes]
+nomes_maiusculos = [nome.upper() for nome in nomes if nome[0] == 'A']
 
 print(nomes_maiusculos)
