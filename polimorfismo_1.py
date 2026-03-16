49010f83f5aa6c92c13df74ea0f99c9200750f46
lphillipe <luisphillipe200@gmail.com>
2026-03-16 18:31:49 -0300

    update polimorfismo_1
    add polimorfismo_2

--- polimorfismo_1.py
+++ polimorfismo_1.py
@@ -8,5 +8,14 @@ class Cachorro(Animal):
     def emitir_som(self):
         print('Au Au!')
 
+class Gato(Animal):
+
+    def emitir_som(self):
+        print('Miau!')
+
 cachorro= Cachorro()
 cachorro.emitir_som()
+
+gato = Gato()
+
+gato.emitir_som()
