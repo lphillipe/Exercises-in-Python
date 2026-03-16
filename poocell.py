d280ca4061d442dd6c12abc6699de700d3b5f3a0
lphillipe <luisphillipe200@gmail.com>
2026-03-16 18:09:22 -0300

    add poocell.py

---
+++ poocell.py
@@ -0,0 +1,20 @@
+class Celular:
+    marca = 'Nokia'
+    modelo = 'Tijolão'
+    cor = 'Azul'
+    tem_camera = False
+    bateria = 'Infinita'
+
+    def fazer_ligações(self):
+        print('Fazendo ligação...')
+
+    def jogar_cobrinha(self):
+        print('Jogando cobrinha...')
+
+    def despertador(self):
+        print('Despertando...')
+
+celular = Celular()
+
+
+celular.despertador()
