905aab99ea3fc2e97ddaff307163a0356cdf0184
lphillipe <luisphillipe200@gmail.com>
2026-03-16 18:45:29 -0300

    update polimorfismo_2

--- polimorfismo_2.py
+++ polimorfismo_2.py
@@ -11,6 +11,14 @@ class Quadrado(Forma):
 
     def area(self):
         return self.lado ** 2
+
+class Circulo(Forma):
+
+    def __init__(self, raio):
+        self.raio = raio
+
+    def area(self):
+        return 3.14 * self.raio ** 2
     
 
 quadrado = Quadrado(5)
@@ -20,3 +28,12 @@ print(area_quadrado)
 quadrado2 = Quadrado(7)
 area_quadrado2 = quadrado2.area()
 print(area_quadrado2)
+
+
+circulo = Circulo(4)
+area_circulo = circulo.area()
+print(area_circulo)
+
+circulo2 = Circulo(6)
+area_circulo2 = circulo2.area()
+print(area_circulo2)
