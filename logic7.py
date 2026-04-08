081143cd97f0f91ac221d98b580bf9e384ac20b4
lphillipe <luisphillipe200@gmail.com>
2026-04-08 07:57:40 -0300

    add logic7.py for calculating the sum of two numbers

---
+++ logic7.py
@@ -0,0 +1,10 @@
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+def soma(a, b):
+    return a + b
+
+total = round(soma(a, b))
+
+print(f"Total da soma é {total}")
+
