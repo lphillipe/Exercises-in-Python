aff4db58cdcfaed7411d90c3a85f3c41fde54d93
lphillipe <luisphillipe200@gmail.com>
2026-04-10 07:45:56 -0300

    add logic8.py for calculating the area of a rectangle

---
+++ logic8.py
@@ -0,0 +1,10 @@
+altura = float(input("Diga a altura do retângulo: "))
+largura = float(input("Diga a largura do retângulo: "))
+
+
+def area(altura, largura):
+    return altura * largura
+
+area_retangulo = area(altura, largura)
+
+print(f"A área do retângulo com altura {altura} e largura {largura} sua area é {area_retangulo}")
