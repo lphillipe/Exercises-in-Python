cca6a1f7744500e1a13d7a85e6dfb55de4cbcedd
lphillipe <luisphillipe200@gmail.com>
2026-04-14 06:55:54 -0300

    add logic9.py for determining if a number is positive, negative, or zero

---
+++ logic9.py
@@ -0,0 +1,8 @@
+num = float(input("Escreva um número: "))
+
+if num < 0:
+    print(f" O {num} é négativo !")
+elif num > 0:
+    print(f"O {num} é positivo !")
+else:
+    print(f" O {num} é zero !")
