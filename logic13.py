700ad720f54548898f1d0e28a39279d11f72bc80
lphillipe <luisphillipe200@gmail.com>
2026-04-16 07:14:05 -0300

    add logic13.py for determining the larger of two numbers

---
+++ logic13.py
@@ -0,0 +1,7 @@
+num1 = float(input("Diga o primeiro número: "))
+num2= float(input("Diga o segundo número: "))
+
+if num1 > num2:
+    print(f"O maior número é {num1:g}.")
+else:
+    print(f"O maior número é {num2:g}.")
