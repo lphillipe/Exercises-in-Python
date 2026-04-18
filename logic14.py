877f70acb41e27ebac7022dded69163aa2eba2be
lphillipe <luisphillipe200@gmail.com>
2026-04-18 20:37:53 -0300

    add logic14.py for comparing two numbers for equality

---
+++ logic14.py
@@ -0,0 +1,7 @@
+num1 = float(input("Diga o primeiro número:"))
+num2 = float(input("Diga o segundo número:"))
+
+if num1 == num2:
+    print("O primeiro e segundo número são iguais.")
+else:
+    print("Os números não são iguais.")
