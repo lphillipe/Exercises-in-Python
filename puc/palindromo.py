93ee76135846f3adbddc53ee27431097c6f4f3f1
lphillipe <luisphillipe200@gmail.com>
2026-05-06 20:19:04 -0300

    refactor: reorganize and implement palindrome and single number functions

---
+++ puc/palindromo.py
@@ -0,0 +1,16 @@
+def palindromo(word):
+    word = word.replace(" ", "").lower()
+    left = 0
+    right = len(word) -1
+
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        
+        left += 1
+        right -= 1
+
+    return True
+
+
+print(palindromo("racecar"))
