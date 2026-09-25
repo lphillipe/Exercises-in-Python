8b2f635a16dc1b0f25cb0edddbebb0a63c6a224d
lphillipe <luisphillipe200@gmail.com>
2026-09-25 07:11:22 -0300

    feat: add new exercise word_lenght

---
+++ get_word.py
@@ -0,0 +1,12 @@
+def word_length(word1, word2):
+    word = word1 + word2
+    return len(word)
+    print("This never gets printed")
+
+length1 = word_length("Rob", "Banks")                     
+length2 = word_length("Barbie", "Kenn")                   
+length3 = word_length("Holly", "Jolley")                  
+
+print("One name is", length1, "letters long.")            
+print("Another name is", length2, "letters long.")        
+print("The final name is", length3, "letters long.") 
