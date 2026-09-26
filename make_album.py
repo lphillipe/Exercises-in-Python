db6908872e8492bed549d4d13648aa82dd24a38d
lphillipe <luisphillipe200@gmail.com>
2026-09-26 14:14:28 -0300

    feat: add new exercise make_album

---
+++ make_album.py
@@ -0,0 +1,9 @@
+def make_album(artist, album):
+
+    albumfull = {'Artist': artist, 'Album': album}
+    return albumfull
+
+
+print(make_album("Avenged Sevenfold", "Nightmare" ))
+print(make_album("Ozzy Osbourne", "Bark At Moon"))
+print(make_album("Trivium", "Vengeance Falls"))
